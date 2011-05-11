58c56a74fdff3f488add82264d3a61b4d6fa72df
Mo Sen <mosen1@gmail.com>
2011-05-11 11:05:48 +1000

    Different approach taken for dualslider utilising one normal slider and one rail-less slider.

--- sandbox/mosen/src/gallery-ebisu-dualslider/demo/index.js
+++ sandbox/mosen/src/gallery-ebisu-dualslider/demo/index.js
@@ -1,5 +1,5 @@
 YUI({
-    filter: 'debug'
+  /*  filter: 'debug' */
 }).use('gallery-ebisu-dualslider', 'console', function(Y) {
         /*
 	var yconsole = new Y.Console({
@@ -10,10 +10,35 @@ YUI({
 
         var slider = new Y.Slider();
         slider.render('.yui3-slider');
+        
+        var slidertwo = new Y.Ebisu.ChildSlider({ parent: slider });
+        slidertwo.render('.yui3-slider');
+        
+        slider._dd.after('drag:align', function(e) {
+            var limiterValue = slidertwo.get('value'),
+                limiterOffset = slidertwo._valueToOffset(limiterValue);
+            
+            //console.dir(e);
+            //Y.log('drag align', 'error', 'slider');
+            if (slider._dd.actXY[0] > limiterOffset) {
+                slider._dd.actXY[0] = limiterOffset - 5;
+            }
+        }, slider._dd);
 
-        var dslider = new Y.Ebisu.DualSlider();
-        //var dslider = new Y.Slider();
-        dslider.render('.yui3-dualslider');
+                
+        slidertwo._dd.after('drag:align', function(e) {
+            var limiterValue = slidertwo.get('value'),
+                limiterOffset = slidertwo._valueToOffset(limiterValue),
+                limitedValue = slider.get('value'),
+                limitedOffset = slider._valueToOffset(limitedValue);
+                
+            if (slidertwo._dd.actXY[0] < limitedOffset) {
+                slider.set('value', limiterValue - 5);
+            }
+        }, slidertwo._dd);
 
+        slider.on('valueChange', function(e) { 
+            Y.log(slider.get('value'));
+        })
         
 });
