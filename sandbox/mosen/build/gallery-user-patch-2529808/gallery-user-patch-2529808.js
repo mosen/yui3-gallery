01b2cfa051dc65c75798b3105867144471ee207c
Mo Sen <mosen1@gmail.com>
2011-05-13 15:00:18 +1000

    fixed a bug where bindUI was not called by the host so event code has moved to the initializer.

--- sandbox/mosen/build/gallery-user-patch-2529808/gallery-user-patch-2529808.js
+++ sandbox/mosen/build/gallery-user-patch-2529808/gallery-user-patch-2529808.js
@@ -6,11 +6,11 @@ YUI.add('gallery-user-patch-2529808', function(Y) {
 // also see: https://github.com/mosen/yui3-gallery/tree/master/sandbox/mosen/src/gallery-dp-datatable
 //
 
-Y.Plugin.DataTableScroll.prototype.injected_bindUI = Y.Plugin.DataTableScroll.prototype.bindUI;
-Y.Plugin.DataTableScroll.prototype.bindUI = function() {
-    this.get('host').after('recordsetChange', Y.bind(this.syncUI, this));
+Y.Plugin.DataTableScroll.prototype.injected_initializer = Y.Plugin.DataTableScroll.prototype.initializer;
 
-    this.injected_bindUI();
+Y.Plugin.DataTableScroll.prototype.initializer = function(config) {
+    this.get('host').after('recordsetChange', Y.bind(this.syncUI, this));
+    this.injected_initializer(config);
 };
 
 Y.Plugin.DataTableScroll.prototype.injected_syncWidths = Y.Plugin.DataTableScroll.prototype._syncWidths;
